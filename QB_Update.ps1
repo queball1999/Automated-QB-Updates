fc283e507937a4c990299475f69c39207b930bcf
Que <57122349+queball1999@users.noreply.github.com>
2024-06-01 15:18:30 -0700

    Update QB_Update.ps1
    Added check for temp update directory
    Removed UAC disable

--- QB_Update.ps1
+++ QB_Update.ps1
@@ -1,3 +1,15 @@
+#
+# Program Name: QB_Update.ps1
+#
+# Description: Powershell script to automate Quickbooks updates
+#
+# Original Author: adamef93  - https://github.com/adamef93/Automated-QB-Updates
+#
+# Editors: Queball1999
+#
+# Date Modified: 1st of June 2024
+#
+
 # Runs as admin
 Write-Host "Checking for elevation... "  
 $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent()) 
@@ -13,45 +25,27 @@ if (($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrato
 }  
 write-host "in admin mode.."
 
+# Check if the C:\IT\QB updates folder exists. If not, create it.
+$folderPath = "C:\IT\QB updates"
+if (-not (Test-Path -Path $folderPath)) {
+    Write-Host "Creating folder: $folderPath"
+    New-Item -ItemType Directory -Path $folderPath
+}
+
 # Disables UAC. The patches require UAC to run and this is included in preparation for automating the install with AutoIT
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+# Currently commenting out as it causes conflict with SIEM - Que 
+#Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
 
 # Change this to your preferred directory. Keep the file name + extension
-$destination = "C:\Admin\QB Updates\qbwebpatch.exe" 
+$destination = "C:\IT\QB Updates\qbwebpatch.exe" 
 # Edit this array to download the versions you need. See README for steps
 $downloads = @(
-# Premier Accountant 2022
-"https://http-download.intuit.com/http.intuit/Downloads/2022/dmknzyq5nUS_R3/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2021
-"https://http-download.intuit.com/http.intuit/Downloads/2021/eo2bf393iUS_R8/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2020
-"https://http-download.intuit.com/http.intuit/Downloads/2020/cveofqqkrsUS_R13/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2019
-"https://http-download.intuit.com/http.intuit/Downloads/2019/szxlidxcipUS_R16/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2018
-"https://http-download.intuit.com/http.intuit/Downloads/2018/qaammbxfvrUS_R17/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2017
-"https://http-download.intuit.com/http.intuit/Downloads/2017/ucnoamocyvUS_R16/Webpatch/qbwebpatch.exe"
-# Premier Accountant 2016
-"https://http-download.intuit.com/http.intuit/Downloads/2016/IFRvyzFmpQUS_R17/WebPatch/qbwebpatch.exe"
-# Premier Accountant 2015
-"https://http-download.intuit.com/http.intuit/Downloads/2015/m734bnderqUS_R17/WebPatch/qbwebpatch.exe"
-# Premier Accountant 2014
-"https://http-download.intuit.com/http.intuit/Downloads/2014/4covjyl0euUS_R16/WebPatch/qbwebpatch.exe"
-# Enterprise Acountant 22
-"https://http-download.intuit.com/http.intuit/Downloads/2022/dmknzyq5nUS_R3/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 21
-"https://http-download.intuit.com/http.intuit/Downloads/2021/eo2bf393iUS_R8/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 20
-"https://http-download.intuit.com/http.intuit/Downloads/2020/cveofqqkrsUS_R13/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 19
-"https://http-download.intuit.com/http.intuit/Downloads/2019/szxlidxcipUS_R16/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 18
-"https://http-download.intuit.com/http.intuit/Downloads/2018/qaammbxfvrUS_R17/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 17
-"https://http-download.intuit.com/http.intuit/Downloads/2017/ucnoamocyvUS_R16/Webpatch/en_qbwebpatch.exe"
-# Enterprise Accountant 16
-"https://http-download.intuit.com/http.intuit/Downloads/2016/IFRvyzFmpQUS_R17/WebPatch/en_qbwebpatch.exe"
+# QuickBooks Desktop Enterprise Solutions 24.0
+"https://http-download.intuit.com/http.intuit/Downloads/2024/rnkpzeq9nUS_R6/Webpatch/en_qbwebpatch.exe"
+# QuickBooks Desktop Enterprise Solutions 23.0
+"https://http-download.intuit.com/http.intuit/Downloads/2023/nctqf0a84US_R12/Webpatch/en_qbwebpatch.exe"
+# QuickBooks Desktop Enterprise Solutions 22.0
+"https://http-download.intuit.com/http.intuit/Downloads/2022/dmknzyq5nUS_R15/Webpatch/en_qbwebpatch.exe"
 )
 
 $downloads | foreach {
@@ -62,43 +56,18 @@ $downloads | foreach {
     # This deletes the downloaded file upon completion of the loop. The update packages are overwritten as new ones are downloaded, but they can be large and don't need to stay       after installation
     Remove-Item -Path $destination -Force 
 }
+
 # This deletes the temp directory created by the patch installer, again to save space
 Remove-Item -Path C:\Windows\Temp\qbwebpatch -Recurse -Force
 
 # Edit this array for the versions you have installed
 $quickbooks = @( 
-# Premier 2022
-"C:\Program Files\Intuit\QuickBooks 2022\QBWPremierAccountant.exe"
-# Premier 2021
-"C:\Program Files (x86)\Intuit\QuickBooks 2021\QBW32PremierAccountant.exe"
-# Premier 2020
-"C:\Program Files (x86)\Intuit\QuickBooks 2020\QBW32PremierAccountant.exe"
-# Premier 2019
-"C:\Program Files (x86)\Intuit\QuickBooks 2019\QBW32PremierAccountant.exe"
-# Premier 2018
-"C:\Program Files (x86)\Intuit\QuickBooks 2018\QBW32PremierAccountant.exe"
-# Premier 2017
-"C:\Program Files (x86)\Intuit\QuickBooks 2017\QBW32PremierAccountant.exe"
-# Premier 2016
-"C:\Program Files (x86)\Intuit\QuickBooks 2016\QBW32PremierAccountant.exe"
-# Premier 2015
-"C:\Program Files (x86)\Intuit\QuickBooks 2015\QBW32PremierAccountant.exe"
-# Premier 2014
-"C:\Program Files (x86)\Intuit\QuickBooks 2014\QBW32PremierAccountant.exe"
-# Enterprise 22
-"C:\Program Files\Intuit\QuickBooks Enterprise Solutions 22.0\QBWEnterpriseAccountant.exe"
-# Enterprise 21
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 21.0\QBW32EnterpriseAccountant.exe"
-# Enterprise 20
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 20.0\QBW32EnterpriseAccountant.exe"
-# Enterprise 19
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 19.0\QBW32EnterpriseAccountant.exe"
-# Enterprise 18
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 18.0\QBW32EnterpriseAccountant.exe"
-# Enterprise 17
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 17.0\QBW32EnterpriseAccountant.exe"
-# Enterprise 16
-"C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 16.0\QBW32EnterpriseAccountant.exe"
+# QuickBooks Enterprise Solutions 24.0
+"C:\Program Files\Intuit\QuickBooks Enterprise Solutions 24.0\QBWEnterpriseProfessional.exe"
+# QuickBooks Enterprise Solutions 23.0
+"C:\Program Files\Intuit\QuickBooks Enterprise Solutions 23.0\QBWEnterpriseProfessional.exe"
+# QuickBooks Enterprise Solutions 22.0
+"C:\Program Files\Intuit\QuickBooks Enterprise Solutions 22.0\QBWEnterpriseProfessional.exe"
 )
 # This loop launches each version in the array above as admin so the patches will apply
 # THIS WILL KILL ALL OPEN VERSIONS OF QUICKBOOKS SO MAKE SURE YOU ARE THE ONLY ONE USING THE SERVER
@@ -106,7 +75,9 @@ $quickbooks | foreach {
     Start-Process $_ -Verb runas
     # Pauses the script to give QB enough time to process the update 
     Start-Sleep -Seconds 20
-    get-process qbw32 | foreach {$_.CloseMainWindow() | Out-Null} | stop-process –force
+    get-process qbw | foreach {$_.CloseMainWindow() | Out-Null} | stop-process –force # updated to qbw as newer versions only run 64 bit.
 }
+
 # Re-enables UAC
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "5"
+# Currently commenting out as it causes conflict with SIEM - Que 
+#Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "5"
